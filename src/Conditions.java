a541af722057d74611006aaf202c56ca1e9356b6
Christian Lisangola <christian.lisangola@gmail.com>
2022-06-08 15:30:04 +0200

    debut

---
+++ src/Conditions.java
@@ -0,0 +1,34 @@
+public class Conditions {
+    public static void main(String[] args) {
+        // declaration de la variable age
+        byte age=31;
+
+        // test  condition
+        // si age >=18 alors
+        // afficher "vous etes majeur donc vous pouvez participer"
+        // fin sin
+        // sinon
+        // afficher "vous etes mineur, vous ne pouvez pas participer"
+        // fin sin
+
+        if(age>=18){
+            System.out.println("vous etes majeur donc vous pouvez participer");
+        }else{
+            System.out.println("vous etes mineur, vous ne pouvez pas participer");
+        }
+
+        String pays="Chine";
+        if(pays=="France"){
+            System.out.println("Bonjour");
+        }else if(pays=="Allemagne"){
+            System.out.println("Guten Tag");
+        }else if(pays=="Espagne"){
+            System.out.println("Holla");
+        }else if(pays=="Chine"){
+            System.out.println("Ninhau");
+        }else{
+            System.out.println("Le pays fourni n'est pas supporte");
+        }
+}
+}
+
