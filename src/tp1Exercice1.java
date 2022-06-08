cef38ba070d2c981108eefc53c19070542b1ae51
Christian Lisangola <christian.lisangola@gmail.com>
2022-06-08 16:33:34 +0200

    dfin de journée

---
+++ src/tp1Exercice1.java
@@ -0,0 +1,19 @@
+public class tp1Exercice1 {
+    public static void main(String[] args) {
+        byte age=13;
+
+        if(age<7) {
+            System.out.println("trop petit");
+        }if(age>=7 && age<=9) {
+            System.out.println("poussin");
+        }if(age>=10 && age<=11) {
+            System.out.println("pupille");
+        }if(age>=12 && age<=13) {
+            System.out.println("benjamin");
+        }if(age>=14 && age<=15) {
+            System.out.println("cadet");
+        }else{
+            System.out.println("trop grand");
+        }
+    }
+}
---
+++ src/tp1Exercice2.java
@@ -0,0 +1,9 @@
+public class tp1Exercice2 {
+    public static void main(String[] args) {
+        float diametre=8,5;
+        float surface=(diametre/2)*(diametre/2)*Math.PI;
+
+
+
+    }
+}
