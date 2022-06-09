5cd8c77f45998980f569d5c8b5a33dae41f89f28
Christian Lisangola <christian.lisangola@gmail.com>
2022-06-09 09:33:09 +0200

    après correction

--- src/tp1Exercice2.java
+++ src/tp1Exercice2.java
@@ -1,9 +1,8 @@
 public class tp1Exercice2 {
     public static void main(String[] args) {
-        float diametre=8,5;
-        float surface=(diametre/2)*(diametre/2)*Math.PI;
-
-
-
+        float diametre=8.5f;
+        float rayon=diametre/2;
+        double surface=rayon*rayon*Math.PI;
+        System.out.println(surface);
     }
 }
