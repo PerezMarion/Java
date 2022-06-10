4a451ba2ce96edeb3aa7ca0d50af5a8208b882da
Christian Lisangola <christian.lisangola@gmail.com>
2022-06-10 10:15:01 +0200

    fin de la correction

--- src/tp3Exercice11.java
+++ src/tp3Exercice11.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class tp3Exercice11 {
     public static void main(String[] args) {
 
-        Scanner input= new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println("Entrer un nombre dont vous voulez calculer la factorielle : ");
-        int nombre= input.nextInt();
+        int nombre = input.nextInt();
         int factorielle = 1;
 
-        for (int i=1;i<=nombre;i=i+1)
-            factorielle = factorielle*i;
-            System.out.println("La factorielle du nombre " +nombre+ " est : " +factorielle);
+        for (int i = 1; i <= nombre; i = i + 1) {
+            factorielle = factorielle * i;
+            System.out.println("La factorielle du nombre " + nombre + " est : " + factorielle);
+        }
     }
 }
