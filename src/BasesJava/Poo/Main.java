85012824cde0fbf31a869d9d1cec1448691dc017
Christian Lisangola <christian.lisangola@gmail.com>
2022-06-15 12:22:29 +0200

    15 juin fin de matinee

--- src/BasesJava/Poo/Main.java
+++ src/BasesJava/Poo/Main.java
@@ -3,21 +3,40 @@ package BasesJava.Poo;
 public class Main {
     public static void main(String[] args) {
 
-    // Etat initial de l'objet après creation
+    // Avant getters et setters
 
-        // Creation d'une instence/objet de la classe Rectangle
+        // Etat initial de l'objet après creation
+
+            // Creation d'une instence/objet de la classe Rectangle
+                // Rectangle terrain=new Rectangle();
+            // Afficher la valeur de l'attribut longueur de l'instence/objet rectangle
+                // System.out.println("La longueur : "+terrain.longueur);
+                // System.out.println("La largeur : "+terrain.largeur);
+
+        // Etat de l'objet après avoir fourni les valeurs
+
+            // terrain.longueur=500;
+            // terrain.largeur=350;
+            // System.out.println("\nLa longueur : "+terrain.longueur);
+            // System.out.println("La largeur : "+terrain.largeur);
+            // System.out.println("La surface : "+terrain.calculSurface());
+            // System.out.println("Le perimetre : "+terrain.calculPerimetre());
+
+    // Apres getters et setters
+
         Rectangle terrain=new Rectangle();
-        // Afficher la valeur de l'attribut longueur de l'instence/objet rectangle
-        System.out.println("La longueur : "+terrain.longueur);
-        System.out.println("La largeur : "+terrain.largeur);
+        System.out.println("La longueur : "+terrain.getLongueur());
+        System.out.println("La largeur : "+terrain.getLargeur());
 
-    // Etat de l'objet après avoir fourni les valeurs
+        // Etat de l'objet après avoir fourni les valeurs
 
-        terrain.longueur=500;
-        terrain.largeur=350;
-        System.out.println("\nLa longueur : "+terrain.longueur);
-        System.out.println("La largeur : "+terrain.largeur);
+        terrain.setLongueur(500);
+        terrain.setLargeur(350);
+        System.out.println("\nLa longueur : "+terrain.getLongueur());
+        System.out.println("La largeur : "+terrain.getLargeur());
         System.out.println("La surface : "+terrain.calculSurface());
         System.out.println("Le perimetre : "+terrain.calculPerimetre());
+
+        terrain.motFin();
     }
 }
--- src/BasesJava/Poo/Rectangle.java
+++ src/BasesJava/Poo/Rectangle.java
@@ -2,14 +2,34 @@ package BasesJava.Poo;
 
 public class Rectangle {
 
-    double longueur;
-    double largeur;
-
+    private double longueur;
+    private double largeur;
     double calculSurface() {
         return longueur * largeur;
     }
-
     double calculPerimetre() {
         return 2*(longueur+largeur);
+    }
+
+    // Methode qui ne sert qu'a afficher quelque chose
+    public void motFin(){
+        System.out.println("C'est fini !");
+    }
+
+    // Accesseurs ou getters : methodes qui donnent la possibilite de lire des attributs prives
+    public double getLongueur() {
+        return longueur;
+    }
+    public double getLargeur() {
+        return largeur;
+    }
+
+    // Manipulateurs ou setters : methodes qui donnent la possibilite de modifier des attributs prives
+    // Creation d'une nouvelle valeur nommée longueur qui va prendre la place de la valeur actuelle du parametre longueur
+    public void setLongueur(double longueur) {
+        this.longueur = longueur;
+    }
+    public void setLargeur(double largeur) {
+        this.largeur = largeur;
     }
 }
